dae50db09c8c41fb5dcafc84b12be5568559b0a5
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-27 23:46:43 +0600

    Red button modified

--- js/events.js
+++ js/events.js
@@ -1,10 +1,11 @@
 // **Another option to using color***//
+// **Purple button
 let purpleButton = document.getElementById('color-purple');
 purpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple'
 }
 
-
+// ****** Red button ******
 let redButton = document.getElementById('color-red');
 redButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'red'
